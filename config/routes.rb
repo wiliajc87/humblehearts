ae6861d17174034a2f0b9496868e4b054d5c0f36
wiliajc87 <jordythepoet@gmail.com>
2015-05-02 17:01:46 -0500

    fixed Karas mistake and made our pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get 'our_story' => 'welcome#our_story'
+  get 'our_services' => 'welcome#our_services'
   resources :donate
   resources :services
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
