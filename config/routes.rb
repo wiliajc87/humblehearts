9277fdbbb873a6aab99dd1cfdf07c23ffaed80ce
wiliajc87 <jordythepoet@gmail.com>
2015-05-02 14:46:29 -0500

    added controller and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  sessions: 'sessions'
+}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
   resources :donate
 
   resources :services
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
