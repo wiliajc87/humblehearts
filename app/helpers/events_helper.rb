729324ccb93427df51ae7b36848b897cc214139f
Sean <nase00@gmail.com>
2015-05-03 04:00:37 -0500

    Production JS compiled. Limit removed from helper.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,16 +2,7 @@ module EventsHelper
 	def albums
 		flickr = Flickr.new(ENV["FLICKR_KEY"]) 
     user = flickr.users('humbleheartsorg@yahoo.com')
-    list = user.photos.first(50)
+    list = user.photos
     arr_one, arr_two, arr_three, arr_four, arr_five = *list.group_by { |x| x[:title] }.map(&:last)
-    # titles = Set.new
-    # albums = Array.new
-    # list.each { |i| titles << i }
-    # titles.each do |title|
-    #   list.each do |album|
-    #     albums << album#y if album["title"] == title
-    #   end
-    # end
-    # albums
 	end
 end
