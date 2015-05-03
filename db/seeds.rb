3ec190d6f1a71860a9d1ff2c6d447f5d4c8ce9a0
karaAJC <KaraAJC@gmail.com>
2015-05-02 22:21:40 -0500

    creating seed file and testing CSV function

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+CSV.foreach("../content/test.csv") do |row|
+  if row[0] == ","
+    next
+  CommunityResource.create(row)
+end
+
+
 t = TeamMember.create(name: 'Shea', title: 'like a boss', photo_url: 'https://scontent-ord.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/11150484_10204278335862435_8430420190725809863_n.jpg?oh=077cdc9bd9c9481f97e7ef526315fe09&oe=55D2324D', bio: 'To avoid this problem, refrain from the following:
 Do not clean your ears with bobby pins, twisted napkin corners, or other long pointed objects.
 Do not use cotton swabs. They will merely push the wax deeper into the ear canal, causing a blockage.
 Do not use ear candles.')
+
