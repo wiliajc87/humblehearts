a01c3d34da767ca4ae330816d2e4f4f3bb6115cf
Sean <nase00@gmail.com>
2015-05-03 03:54:37 -0500

    Allow for up to 5 galleries

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,6 +3,15 @@ module EventsHelper
 		flickr = Flickr.new(ENV["FLICKR_KEY"]) 
     user = flickr.users('humbleheartsorg@yahoo.com')
     list = user.photos.first(50)
-    array_one, array_two = *list.group_by { |x| x[:title] }.map(&:last)
+    arr_one, arr_two, arr_three, arr_four, arr_five = *list.group_by { |x| x[:title] }.map(&:last)
+    # titles = Set.new
+    # albums = Array.new
+    # list.each { |i| titles << i }
+    # titles.each do |title|
+    #   list.each do |album|
+    #     albums << album#y if album["title"] == title
+    #   end
+    # end
+    # albums
 	end
 end
