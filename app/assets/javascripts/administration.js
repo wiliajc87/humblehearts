1ba38a7518fd59d11473590f1b47dc4b3dbb0954
Shea <sstrausman@gmail.com>
2015-05-08 13:49:01 -0500

    removing last reference to DOM.

--- app/assets/javascripts/administration.js
+++ app/assets/javascripts/administration.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
             var tabNum = $this.index();
             var nthChild = tabNum+1;
             $("#tabs li.active").removeClass("active");
-            $(this).addClass("active");
+            $this.addClass("active");
             $("#tab li.active").removeClass("active");
             $("#tab li:nth-child("+nthChild+")").addClass("active");
         }
