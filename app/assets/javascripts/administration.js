65ea7b52c76b00137acb684d7608ef97f572f9b9
Shea <sstrausman@gmail.com>
2015-05-08 13:05:51 -0500

    added saved this variable to administration.js

--- app/assets/javascripts/administration.js
+++ app/assets/javascripts/administration.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $("#tabs li").click(function(){
+        var $this = $(this);
         if (!$(this).hasClass("active")) {
             var tabNum = $(this).index();
             var nthChild = tabNum+1;
