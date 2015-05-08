9cb704f4ee1858f369a1480f42af3a99ace47bfa
Shea <sstrausman@gmail.com>
2015-05-08 10:17:03 -0500

    fixing API issue

---
+++ app/assets/javascripts/administration.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#tabs li").click(function(){
+        if (!$(this).hasClass("active")) {
+            var tabNum = $(this).index();
+            var nthChild = tabNum+1;
+            $("#tabs li.active").removeClass("active");
+            $(this).addClass("active");
+            $("#tab li.active").removeClass("active");
+            $("#tab li:nth-child("+nthChild+")").addClass("active");
+        }
+    });
+});
