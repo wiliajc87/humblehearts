acdd325f85ad3cc341749df74965c84cfebeb76f
Shea <sstrausman@gmail.com>
2015-05-08 13:40:48 -0500

    finished jscript refactor

--- app/assets/javascripts/administration.js
+++ app/assets/javascripts/administration.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $("#tabs li").click(function(){
         var $this = $(this);
-        if (!$(this).hasClass("active")) {
-            var tabNum = $(this).index();
+        if (!$this.hasClass("active")) {
+            var tabNum = $this.index();
             var nthChild = tabNum+1;
             $("#tabs li.active").removeClass("active");
             $(this).addClass("active");
